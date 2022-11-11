8dd195343ed91afd4d474297335447e81258cb75
Juan Diego Jurado <juandiego_jp@hotmail.com>
2022-11-11 01:38:41 +0100

    SOLUCIONADO EL PROBLEMA CON TAILWIND

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/output.css" rel="stylesheet">
+    <link href="css/output.css" rel="stylesheet">
     <title>Tienda</title>
 </head>
 
@@ -97,7 +97,7 @@
         </table>
         <p>Número total de filas: <?= hh($total) ?></p>
     </div>
-    <script src="/js/flowbite.js"></script>
+    <script src="js/flowbite.js"></script>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html, php}", "./node_modules/flowbite/**/*.js"],
+  content: ["./public/**/*.{html,php}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
